0a34d686909d72a1fd86d1d6780d344e3e3efd92
slowmountain <panos.papiotis@gmail.com>
2017-02-14 18:20:02 +0100

    added redirect URL argument in main example

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 
 int main() {
     // Create a repovizz2_client object
-    repovizz2_client my_client(U("43e7a909-6011-47e2-b58e-200e72bd8b06"), U("33d728c7-a5ab-4406-86e6-9702c6546a5e"));
+    // You must provide the client id, secret, and redirect URL
+    repovizz2_client my_client(U("43e7a909-6011-47e2-b58e-200e72bd8b06"), U("33d728c7-a5ab-4406-86e6-9702c6546a5e"), U("http://localhost:5001"));
 
     // Authorize client
     my_client.authorize();
